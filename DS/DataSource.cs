ed6f1e4e89be28897a3e10f4dab49b7ad7bf31f7
izhchako <rachel16188@gmail.com>
2020-03-23 15:21:46 +0200

    initial realese

---
+++ DS/DataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    public class DataSource
+    {
+        public static List<BE.Nanny> nannies_List=new List<BE.Nanny>();
+        public static List<BE.Mother> mothers_List=new List<BE.Mother>();
+        public static List<BE.Child> children_List=new List<BE.Child>();
+        public static List<BE.Contract> contracts_List= new List<BE.Contract>();
+    }
+}
